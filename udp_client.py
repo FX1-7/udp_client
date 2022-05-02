e78bd63eab1cfd93b24b314a0c620c1da0cda174
Keiranos <31273773+Keiranos@users.noreply.github.com>
2022-05-02 22:21:17 +0100

    Comments

--- udp_client.py
+++ udp_client.py
@@ -6,17 +6,17 @@ Date: 02/05/2022
 '''
 
 import socket
-SERVER_NAME = 'localhost'
-SERVER_PORT = 11001
+SERVER_NAME = '127.0.0.1' # Server address
+SERVER_PORT = 11001 # Server port
 
-CLIENT_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+CLIENT_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create the connection
 
-USER_MESSAGE = input('Please enter a calculation to send to the server:\n')
+USER_MESSAGE = input('Please enter a calculation to send to the server:\n') # Get the calculation from the user
 
-CLIENT_SOCKET.sendto(USER_MESSAGE.encode(), (SERVER_NAME, SERVER_PORT))
+CLIENT_SOCKET.sendto(USER_MESSAGE.encode(), (SERVER_NAME, SERVER_PORT)) # Send the calculation the server
 
-MODIFIED_MESSAGE, SERVER_ADDRESS = CLIENT_SOCKET.recvfrom(2048)
+MODIFIED_MESSAGE, SERVER_ADDRESS = CLIENT_SOCKET.recvfrom(2048) # get the response
 
-print("\nAnswer:")
-print(MODIFIED_MESSAGE.decode())
-CLIENT_SOCKET.close()
+print("\nAnswer:") # Print out the answer
+print(MODIFIED_MESSAGE.decode()) # In a nice format
+CLIENT_SOCKET.close() # Close the connection
